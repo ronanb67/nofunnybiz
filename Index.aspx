06bca914c6ad741e3c2933f7044dce0b96110832
ronanb67 <ronan.byrne@mhlabs.net>
2014-02-14 11:09:55 +0000

    added twilio code

---
+++ Index.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+   
+    <div class="hero-unit" style="background-color:white">
+    <div class="container">
+        <a href="#" class="btn btn-large btn-danger" Call us!<i class="fa fa-phone fa-1x"></i></a>
+    
+        </div>
+        </div>
+</asp:Content>
+
---
+++ Index.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System;
+using Twilio;
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Auth Token at twilio.com/user/account 
+        string AccountSid = "AC45b00a5504e242b8a486ebf4cad405c9";
+        string AuthToken = "e0d2edd31902f751871bd792a18a06b3";
+        var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+        // Build the parameters 
+        var options = new CallOptions();
+        options.To = "";
+        options.From = "+353766805789";
+        options.ApplicationSid = "PN12025fa386226d4ead7911686fa78b7a";
+        options.Method = "GET";
+        options.FallbackMethod = "GET";
+        options.StatusCallbackMethod = "GET";
+        options.Record = false;
+
+        var call = twilio.InitiateOutboundCall(options);
+        Console.WriteLine(call.Sid);
+    }
+}
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
