99bcf2c253d064a65fe911d85b1b869799940fff
ronanb67 <ronan.byrne@mhlabs.net>
2014-02-18 11:27:58 +0000

    added twilio code

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -4,20 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System;
 using Twilio;
 class Example
 {
     static void Main(string[] args)
     {
-        // Find your Account Sid and Auth Token at twilio.com/user/account 
-        string AccountSid = "AC45b00a5504e242b8a486ebf4cad405c9";
-        string AuthToken = "e0d2edd31902f751871bd792a18a06b3";
+        
+    }
+}
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void callpage_Click(object sender, EventArgs e)
+    { // Find your Account Sid and Auth Token at twilio.com/user/account 
+        string AccountSid = "AC15403cb368014b2d1eb45fa4e366343e";
+        string AuthToken = "db65726686db0599044da6e2664f5dba";
         var twilio = new TwilioRestClient(AccountSid, AuthToken);
 
         // Build the parameters 
         var options = new CallOptions();
-        options.To = "+353862240661";
+        options.To = "+35316972147";
         options.From = "+353766805789";
         options.ApplicationSid = "PN12025fa386226d4ead7911686fa78b7a";
         options.Method = "GET";
@@ -27,12 +36,7 @@ class Example
 
         var call = twilio.InitiateOutboundCall(options);
         Console.WriteLine(call.Sid);
-    }
-}
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
 
+        Server.Transfer("Camera.aspx", true);
     }
 }
