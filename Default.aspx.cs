8a31db81e029d75e6973b5a7d2450d4a1e4fe8e9
ronanb67 <ronan.byrne@mhlabs.net>
2014-02-19 16:50:06 +0000

    removed console.writeline from end of code

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,35 +7,39 @@ using System.Web.UI.WebControls;
 using Twilio;
 
 
-    public partial class _Default : System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
+
+    }
+    protected void callpage_Click(object sender, EventArgs e)
+    { // Find your Account Sid and Auth Token at twilio.com/user/account 
+        string AccountSid = "AC15403cb368014b2d1eb45fa4e366343e";
+        string AuthToken = "db65726686db0599044da6e2664f5dba";
+        var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+
+        // Build the parameters 
+        CallOptions options = new CallOptions();
+        options.To = "+353862240661";
+        options.From = "+353766805789";
+        //options.Url = "http://demo.twilio.com/docs/voice.xml";
+        options.ApplicationSid = "APe287283110f74e87eaefde2b990398c6";
+        options.Method = "GET";
+        options.FallbackMethod = "GET";
+        options.StatusCallbackMethod = "GET";
+        options.Record = false;
+
+        var call = twilio.InitiateOutboundCall(options);        
+        if (call.RestException == null)
         {
-
+            Response.Write(string.Format("Started call: {0}", call.Sid));
         }
-        protected void callpage_Click(object sender, EventArgs e)
-        { // Find your Account Sid and Auth Token at twilio.com/user/account 
-            string AccountSid = "AC15403cb368014b2d1eb45fa4e366343e";
-            string AuthToken = "db65726686db0599044da6e2664f5dba";
-            var twilio = new TwilioRestClient(AccountSid, AuthToken);
-            Twilio.Account account = twilio.GetAccount();
-            string APIversuion = twilio.ApiVersion;
-            string TwilioBaseURL = twilio.BaseUrl;
-
-            // Build the parameters 
-            CallOptions options = new CallOptions();
-            options.To = "+353862240661";
-            options.From = "+353766805789";
-            //options.Url = "http://demo.twilio.com/docs/voice.xml";
-            options.ApplicationSid = "APe287283110f74e87eaefde2b990398c6";
-            options.Method = "GET";
-            options.FallbackMethod = "GET";
-            options.StatusCallbackMethod = "GET";
-            options.Record = false;
-
-            var call = twilio.InitiateOutboundCall(options);
-            Console.WriteLine(call.Sid);
-
-
+        else
+        {
+            Response.Write(string.Format("Error: {0}", call.RestException.Message));
         }
     }
+}
+
