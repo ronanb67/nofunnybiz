5501d71ff56838a3f5f48454a967eded75667af1
ronanb67 <ronan.byrne@mhlabs.net>
2014-02-26 16:14:01 +0000

    added twiml url

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -24,22 +24,23 @@ public partial class _Default : System.Web.UI.Page
         CallOptions options = new CallOptions();
         options.To = "+353862240661";
         options.From = "+353766805789";
-        //options.Url = "http://demo.twilio.com/docs/voice.xml";
+        options.Url = "http://demo.twilio.com/docs/voice.xml";
         options.ApplicationSid = "APe287283110f74e87eaefde2b990398c6";
         options.Method = "GET";
         options.FallbackMethod = "GET";
         options.StatusCallbackMethod = "GET";
         options.Record = false;
 
-        var call = twilio.InitiateOutboundCall(options);        
-        if (call.RestException == null)
-        {
-            Response.Write(string.Format("Started call: {0}", call.Sid));
-        }
-        else
-        {
-            Response.Write(string.Format("Error: {0}", call.RestException.Message));
-        }
+        var call = twilio.InitiateOutboundCall(options);
+        Console.WriteLine(call.Sid);
+        //if (call.RestException == null)
+        //{
+        //    Response.Write(string.Format("Started call: {0}", call.Sid));
+        //}
+        //else
+        //{
+        //    Response.Write(string.Format("Error: {0}", call.RestException.Message));
+        //}
     }
 }
 
